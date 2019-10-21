7a61e552ff54bc0a13a09e844fc07f8cfcccd24e
Anthon Holmqvist <anthon.holmqvist@softhouse.se>
2019-10-21 21:40:19 +0200

    Track built files
    Update to track built files in docs directory.
    This is so that GitHub pages can serve the
    output from the docs folder.

--- vue.config.js
+++ vue.config.js
@@ -3,27 +3,5 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // vue.config.js
 module.exports = {
-  outputDir: 'public',
-  chainWebpack(config) {
-    config
-      .plugin('html')
-      .tap(args => {
-        return args.map(arg => {
-          arg.template = './static/index.html';
-          return arg;
-        });
-      });
-  },
-  configureWebpack: {
-    plugins: [
-      new CopyWebpackPlugin(
-        [{
-          from: './static',
-          to: './',
-          toType: 'dir',
-          ignore: ['.DS_Store'],
-        }],
-      ),
-    ],
-  },
+  outputDir: 'docs',
 };
